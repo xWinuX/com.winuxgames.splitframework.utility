863d9a95860555e0bd859b406531dfe83e95dc63
WinuX <37900754+xWinuX@users.noreply.github.com>
2022-05-04 01:01:41 +0200

    Added Coroutine Utility

---
+++ Runtime/CoroutineUtility.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections;
+using UnityEngine;
+
+namespace WinuXGames.SplitFramework.Utility
+{
+    public static class CoroutineUtility
+    {
+        public static IEnumerator WaitForOneFrame(Action action)
+        {
+            yield return new WaitForEndOfFrame();
+            action.Invoke();
+        }
+    }
+}
