8105a7195965c5eb16f8245279666579482ce313
WinuX <37900754+xWinuX@users.noreply.github.com>
2022-05-15 18:26:16 +0200

    added mathutility with mod function

---
+++ Runtime/MathUtility.cs
@@ -0,0 +1,14 @@
+﻿namespace WinuXGames.SplitFramework.Utility
+{
+    public class MathUtility
+    {
+        /// <summary>
+        /// A modulo function that works with negative numbers
+        /// Regular % mod is still faster so only use this for negative numbers
+        /// </summary>
+        /// <param name="a"></param>
+        /// <param name="b"></param>
+        /// <returns>a mod b</returns>
+        public static int Mod(int a, int b) => (a % b + b) % b;
+    }
+}
